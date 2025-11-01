8a2b4ed66c0c990f2bd3f2862bea1a760603585e
Gavin Wyant <gavincwyant@gmail.com>
2025-11-01 19:12:55 -0400

    init commit for my firewall project written in C. currently I am using Berkely Packet filter (/dev/bpf0) to sniff all packets on my network. I currently have formatting for tcp and upd as well as a raw hex dump function

---
+++ fire.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <sys/poll.h>
+#include <sys/ioctl.h>
+#include <net/bpf.h>
+#include <net/if.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <netinet/udp.h>
+#include <netinet/tcp.h>
+#include <netinet/ip.h>
+#include <netinet/if_ether.h>
+
+
+
+#define PORT 1234
+#define HOST "127.0.0.1"
+#define BUF_LEN 1024000
+
+typedef enum {CONN_SOCK, CLIENT_SOCK, BIND, LISTEN, BPF}ErrorType;
+
+void handle_error(ErrorType);
